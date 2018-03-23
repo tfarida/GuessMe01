b84a750eca88b9ae7fc748044b9305652b2b9e3d
tfarida <37550133+tfarida@users.noreply.github.com>
2018-03-22 20:53:48 -0500

    update db script
    delete unused db script and add some new

--- WebContent/WEB-INF/lib/db/db_query/getOnlineGamers_1.0.sql
+++
@@ -1,6 +0,0 @@
-SELECT L.id, L.sessionId, L.loginAt, L.logoutAt, L.activeStatus
-, L.userId, U.gamerName, U.emailAddress, U.password, U.fullName, U.gender, U.createdAt  
-FROM tb_user_login L 
-left join tb_user U on L.userId = U.id
-where L.activeStatus = 1
-group by L.userId
